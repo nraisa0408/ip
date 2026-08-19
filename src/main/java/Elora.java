083a16ab09bbc412d7b170b3bb82eaedad7f143a
Nishat Raisa <e1591059@u.nus.edu>
2026-08-20 00:38:46 +0800

    Implement unmark command

--- src/main/java/Elora.java
+++ src/main/java/Elora.java
@@ -39,6 +39,11 @@ public class Elora {
                 isDone[index] = true;
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  [X] " + tasks[index]);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                isDone[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[index]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
