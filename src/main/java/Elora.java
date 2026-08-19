1f376edce24dfda5f3ffa3d979d791b4e3dadcb5
Nishat Raisa <e1591059@u.nus.edu>
2026-08-19 23:50:35 +0800

    Rename Duke to Elora

---
+++ src/main/java/Elora.java
@@ -0,0 +1,10 @@
+public class Elora {
+    public static void main(String[] args) {
+        String logo = " _____ _\n"
+                + "| ____| | ___  _ __ __ _\n"
+                + "|  _| | |/ _ \\| '__/ _` |\n"
+                + "| |___| | (_) | | | (_| |\n"
+                + "|_____|_|\\___/|_|  \\__,_|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
