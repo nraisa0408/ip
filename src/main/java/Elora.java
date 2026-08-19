64b88cd2a11e3cf307e11ba7f9453e17d62dc460
Nishat Raisa <e1591059@u.nus.edu>
2026-08-20 00:17:45 +0800

    Add support for echo

--- src/main/java/Elora.java
+++ src/main/java/Elora.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Elora {
     public static void main(String[] args) {
         String logo = " _____ _\n"
@@ -5,17 +7,26 @@ public class Elora {
                 + "|  _| | |/ _ \\| '__/ _` |\n"
                 + "| |___| | (_) | | | (_| |\n"
                 + "|_____|_|\\___/|_|  \\__,_|\n";
-
         String line = "____________________________________________________________";
 
         System.out.println(line);
         System.out.println(logo);
-        System.out.println("Hello! I'm Elora.");
+        System.out.println("Hello! I'm Elora - part friend, part philosopher, part guide.");
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println("Bye for now, friend. Until our paths cross again!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println("I hear you: " + input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
