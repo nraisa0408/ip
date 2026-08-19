dc69ee851b0ca36f5b3e556c00574e4cb4b2d849
Nishat Raisa <e1591059@u.nus.edu>
2026-08-20 00:37:54 +0800

    Implement mark command

--- src/main/java/Elora.java
+++ src/main/java/Elora.java
@@ -16,6 +16,7 @@ public class Elora {
         System.out.println(line);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -28,9 +29,16 @@ public class Elora {
                 System.out.println(line);
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                isDone[index] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + tasks[index]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
