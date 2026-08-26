e75a6152497d49dbc5e9d032afab568c06c6fc3f
Nishat Raisa <e1591059@u.nus.edu>
2026-08-26 17:46:38 +0800

    Save tasks to disk when list changes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,6 +23,10 @@ public class Task {
         return description;
     }
 
+    public String toSaveFormat() {
+        return (isDone ? "1" : "0") + " | " + description;
+    }
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
